986c171778683e01e2f8e861355dbb5ae6438c2d
JuanGraell <jgraell@unal.edu.co>
2024-04-12 13:19:52 -0500

    Fetch: Added Module Model & Migration

---
+++ database/migrations/2024_04_12_181754_create_modules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('modules', function (Blueprint $table) {
+            $table->id('moduleId');
+            $table->string('title');
+            $table->string('description');
+            $table->unsignedBigInteger('TeacherUser');
+            $table->foreign('TeacherUser')->references('teacherUser')->on('teachers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('modules');
+    }
+};
