825d7e5b23b15ccd11f14b7aec7f5c646fb119ff
JuanGraell <jgraell@unal.edu.co>
2024-04-12 12:49:00 -0500

    Fetch: Added User Model & Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->string('username')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role');
+            $table->string('institutionalId');
             $table->rememberToken();
             $table->timestamps();
         });
