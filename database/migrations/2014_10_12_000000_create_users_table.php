58e3ece2456d8d51abb2c745b55a1915f690edb4
JuanGraell <jgraell@unal.edu.co>
2024-04-12 12:53:57 -0500

    Hotfix: Foreign keys on user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');
-            $table->string('institutionalId');
+            $table->foreign('institutionalId')->references('id')->on('educationalInstitution');
             $table->rememberToken();
             $table->timestamps();
         });
