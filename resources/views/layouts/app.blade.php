be510f271a5491f828fb7ea79f7c84777be743f3
JuanGraell <jgraell@unal.edu.co>
2024-08-16 15:48:53 -0500

    style: :art: improved appearance of 'Iniciar Sesión' and 'Registrarse' buttons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,17 +38,7 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                            <!-- Se eliminaron login y register del navbar -->
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
