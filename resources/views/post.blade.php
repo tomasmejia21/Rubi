0582fadeb4ebb26302035a0e769157bdd9ce8964
tomasmejia21 <tmejiag@unal.edu.co>
2024-08-26 21:25:57 -0500

    fix: fixed styles in post view and added exceptions to create post view

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rubí - Administrar alumnos</title>
+    <title>Rubi - Administrar Posts</title>
     <link rel="stylesheet" href="{{ asset('css/blogStyles/Styles.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
@@ -38,21 +38,24 @@
                                     <div class="mb-3">
                                         <label for="title" class="form-label">Titulo</label>
                                         <input type="text" name="title" id="title" class="form-control">
+                                        <span id="error-message-title"></span>
                                     </div>
                                     <div class="mb-3">
-                                        <label for="body" class="form-label">Body:</label>
+                                        <label for="body" class="form-label">Body</label>
                                         <textarea id="body" name="body" class="form-control" rows="3"></textarea>
+                                        <span id="error-message-body"></span>
                                     </div>
                                     <div class="mb-3">
                                         <label for="image" class="form-label">Imagen</label>
                                         <input class="form-control" type="file" id="image" name="image">
+                                        <span id="error-message-image"></span>
                                     </div>
                                     
                                     
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Volver</button>
-                                    <button type="submit" class="btn btn-primary">Crear</button>
+                                    <button type="submit" class="btn" id="submit-button" disabled>Crear</button>
                                 </div>
                             </div>
                         </div>
@@ -99,5 +102,6 @@
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/adminExceptions/adminPostsExceptions.js')}}"></script>
 </body>
 </html>
