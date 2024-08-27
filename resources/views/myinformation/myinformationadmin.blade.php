d49323073bde87075a25f69ca13502262b148143
tomasmejia21 <tmejiag@unal.edu.co>
2024-08-26 21:09:52 -0500

    fix: fixed header in myinformationadmin view

--- resources/views/myinformation/myinformationadmin.blade.php
+++ resources/views/myinformation/myinformationadmin.blade.php
@@ -1,3 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi información</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
+</head>
 @include('partials.rubiHeader')
     <br>
     <div class="container text-center text-white">
