bcb46e8ea123573c81151742813f905283d72b63
tomasmejia21 <tmejiag@unal.edu.co>
2024-09-15 20:01:28 -0500

    fix: Fixed some hrefs and removed innecesary buttons and links.

--- resources/views/myinformation/myinformationadmin.blade.php
+++ resources/views/myinformation/myinformationadmin.blade.php
@@ -25,21 +25,19 @@
                 @endforeach
             </div>
             <div class="col-4">
-                <h3>Blogs</h3>
-                <a href="">Entradas blog</a>
+                <h3>Blog</h3>
+                <a href="/blog">Ver posts</a>
                 <br>
-                <a href="">Mensajes en foros</a>
-                <br>
-                <a href="">Foros de discusion</a>
+                <a href="/blog/create">Gestionar posts</a>
             </div>
             <div class="col-4">
                 <h3>Modulos</h3>
-                <a href="">Mis modulos</a>
+                <a href="/modules">Mis modulos</a>
             </div>
         </div>
     </div>
 
-    <script src="{{ asset('js/myinfoExceptions.js')}}"></script>    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>  
+    <script src="{{ asset('js/myinfoExceptions.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
