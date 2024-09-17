2532e133ae50f278dd16b7e310669d80211292f0
tomasmejia21 <tmejiag@unal.edu.co>
2024-09-16 19:54:00 -0500

    feat: :sparkles: Added a view that contains the downloaded information

---
+++ resources/views/reports/teacherReports.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Usuarios Inscritos en el Módulo</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Email</th>
+                <th>Rol</th>
+                <th>Institución Educativa</th>
+                <th>Fecha de Registro</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($students as $student)
+                <tr>
+                    <td>{{ $student->id }}</td>
+                    <td>{{ $student->name }}</td>
+                    <td>{{ $student->email }}</td>
+                    <td>{{ $student->role->name }}</td>
+                    <td>{{ $student->educationalinstitution->name }}</td>
+                    <td>{{ $student->created_at->format('d/m/Y') }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
