adf881ec448bf09a000564c6452fd3ff18ea00ab
JuanGraell <jgraell@unal.edu.co>
2024-09-26 21:06:14 -0500

    feat: :sparkles: Implemented the api for rubi

--- app/Http/Controllers/EducationalInstitutionController.php
+++ app/Http/Controllers/EducationalInstitutionController.php
@@ -7,94 +7,48 @@ use App\Models\EducationalInstitution;
 
 class EducationalInstitutionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $institution = EducationalInstitution::all();
-        return view('admin.adminEducationalInstitution')->with('institution', $institution);
+        return EducationalInstitution::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('admin.createEducationalInstitution');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        // Se crea una instancia
         $institution = new EducationalInstitution();
 
-        // Se genera el ID de la institucion educativa
-        $date = date('Ymd'); // Obtiene la fecha actual en formato YYYYMMDD
-        $count = EducationalInstitution::where('institutionalId', 'like', $date.'%')->count(); // Cuenta los profesores creados hoy
-        $institutionalId = $date . str_pad($count, 1, '0', STR_PAD_LEFT); // Añade el contador al final de la fecha
+        $date = date('Ymd');
+        $count = EducationalInstitution::where('institutionalId', 'like', $date.'%')->count();
+        $institutionalId = $date . str_pad($count, 1, '0', STR_PAD_LEFT);
 
-        // Verifica si el ID ya existe en la base de datos
         while (EducationalInstitution::where('institutionalId', $institutionalId)->exists()) {
-            // Si el ID ya existe, incrementa el contador y genera un nuevo ID
             $count++;
             $institutionalId = $date . str_pad($count, 1, '0', STR_PAD_LEFT);
         }
 
-        // Se asigna como si fuese un constructor
-        // Base de datos = los datos del formulario
         $institution->institutionalId = $institutionalId;
-        $institution->name = $request->name;
-        $institution->address = $request->address;
-        $institution->city = $request->city;
-        $institution->country = $request->country;
+        $institution->fill($request->all());
         $institution->save();
-        return redirect()->route('EducationalInstitution.index');
 
+        return response()->json($institution, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show($institutionalId)
     {
-        //
+        $institution = EducationalInstitution::findOrFail($institutionalId);
+        return $institution;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function update(Request $request, $institutionalId)
     {
-        $institution = EducationalInstitution::find($id);
-        return view('admin.editEducationalInstitution')->with('institution', $institution);
+        $institution = EducationalInstitution::findOrFail($institutionalId);
+        $institution->update($request->all());
+        return $institution;
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function destroy($institutionalId)
     {
-        // Se hace el Update en la base de datos
-        $institution = EducationalInstitution::find($id);
-        $institution->name = $request->name;
-        $institution->address = $request->address;
-        $institution->city = $request->city;
-        $institution->country = $request->country;
-        $institution->save();
-        return redirect()->route('EducationalInstitution.index');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        $EducationalInstitution = EducationalInstitution::find($id);
-        $EducationalInstitution->delete();
-        return redirect()->route('EducationalInstitution.index');
+        $institution = EducationalInstitution::findOrFail($institutionalId);
+        $institution->delete();
+        return response()->json(['message' => 'The institution successfully deleted'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EducationalInstitutionController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Administrar instituciones educativas - Solo administradores
+// Route::middleware(['role:administrator'])->group(function () {
+//     Route::apiResource('educationalinstitutions', EducationalInstitutionController::class);
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('educationalinstitutions', EducationalInstitutionController::class);
